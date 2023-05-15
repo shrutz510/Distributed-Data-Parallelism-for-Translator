2038b51e6575e5d424e5634ccbfecc670cb0d9fd
Shruti Wagle <43674888+shrutz510@users.noreply.github.com>
2023-05-15 03:07:25 -0400

    Update multi.sh

--- Batch/multi.sh
+++ Batch/multi.sh
@@ -18,17 +18,6 @@ singularity exec --nv \
 singularity exec --nv \
             --overlay /scratch/sgw6735/ddpProject/my_pytorch.ext3:ro \
             /scratch/work/public/singularity/cuda11.6.124-cudnn8.4.0.27-devel-ubuntu20.04.4.sif\
-            /bin/bash -c "source /ext3/env.sh; python multi_run.py 15 5 64 1"
-
-
-singularity exec --nv \
-            --overlay /scratch/sgw6735/ddpProject/my_pytorch.ext3:ro \
-            /scratch/work/public/singularity/cuda11.6.124-cudnn8.4.0.27-devel-ubuntu20.04.4.sif\
-            /bin/bash -c "source /ext3/env.sh; python multi_run.py 15 5 128 1"
-
-singularity exec --nv \
-            --overlay /scratch/sgw6735/ddpProject/my_pytorch.ext3:ro \
-            /scratch/work/public/singularity/cuda11.6.124-cudnn8.4.0.27-devel-ubuntu20.04.4.sif\
             /bin/bash -c "source /ext3/env.sh; python multi_run.py 15 5 32 2"
 
 singularity exec --nv \
